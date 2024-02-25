ecc8986401d098659929d7e99dfe7495a8e3bb1c
Wemand <110137781+Wemand@users.noreply.github.com>
2024-02-25 23:48:32 +0300

    BasePreAlpha

--- Megalistonator3k/MainWindow.xaml.cs
+++ Megalistonator3k/MainWindow.xaml.cs
@@ -1,14 +1,6 @@
 ﻿using System.IO;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Megalistonator3k
 {
@@ -116,7 +108,7 @@ namespace Megalistonator3k
             }
 
         }
-
+        
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
             foreach (var folder in titleFoldersList)
